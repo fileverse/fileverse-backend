dd947e29a6def77677ca12d3cba4c8a600cf0fb8
Aakash Jakhmola <aakash001jakhmola@gmail.com>
2022-02-15 10:06:58 +0530

    added comment schema

---
+++ src/infra/database/models/schema/comment.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const _comment = {};
+
+_comment.schema = new Schema(
+  {
+    shortId: { type: String, required: true },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'accounts',
+    },
+    file: {
+      type: Schema.Types.ObjectId,
+      ref: 'files',
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    fileUuid: { type: String },
+    by: { type: String },
+    // system generated
+    createdAt: { type: Number, required: true, default: Date.now },
+    updatedAt: { type: Number, default: Date.now },
+  },
+  { usePushEach: true },
+  { runSettersOnQuery: true },
+);
+
+_comment.schema.pre('save', function (next) {
+  const comment = this;
+  comment.updatedAt = Date.now();
+  next();
+});
+
+_comment.schema.methods.safeObject = function () {
+  const safeFields = [
+    '_id',
+    'shortId',
+    'user',
+    'file',
+    'text',
+    'fileUuid',
+    'by',
+    'createdAt',
+    'updatedAt',
+  ];
+  const newSafeObject = {};
+  safeFields.forEach((elem) => {
+    // eslint-disable-next-line security/detect-object-injection
+    newSafeObject[elem] = this[elem];
+  });
+  return newSafeObject;
+};
+
+_comment.model = mongoose.model('comments', _comment.schema);
+
+module.exports = _comment;
