65af37e783a1f31df82a1e6b6883e4603a532fdd
Aakash Jakhmola <aakash001jakhmola@gmail.com>
2022-02-21 17:51:37 +0530

    added description in interface

--- src/domain/file/edit.js
+++ src/domain/file/edit.js
@@ -5,7 +5,7 @@ const MoralisService = require('../../infra/utils/moralis');
 
 const moralisService = new MoralisService();
 
-async function edit(uuid, { name, file, token, address }) {
+async function edit(uuid, { name, file, token, address, description }) {
   const foundFile = await File.findOne({ uuid });
   if (!foundFile) {
     return ErrorHandler.throwError({
@@ -45,6 +45,9 @@ async function edit(uuid, { name, file, token, address }) {
   }
   if (name) {
     foundFile.name = name;
+  }
+  if (description) {
+    foundFile.description = description;
   }
   await foundFile.save();
   return foundFile.safeObject();
--- src/interface/rest/file/create.js
+++ src/interface/rest/file/create.js
@@ -5,14 +5,16 @@ const { Joi, validate } = validator;
 const createValidation = {
   body: Joi.object({
     name: Joi.string().required(),
+    description: Joi.string().optional().max(500),
   }),
 };
 
 async function create(req, res) {
-  const { name } = req.body;
+  const { name, description } = req.body;
   const createdFile = await File.create({
     file: req.files.file,
     name,
+    description,
     owner: req.userId,
   });
   res.json(createdFile);
