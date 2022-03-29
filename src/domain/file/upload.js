433bc2923208e5d27c2e6bfb0f3cfd4fc290266b
Aakash-Jakhmola <aakash001jakhmola@gmail.com>
2022-03-29 14:53:55 +0530

    add web3 storage

--- src/domain/file/upload.js
+++ src/domain/file/upload.js
@@ -4,20 +4,33 @@ const S3 = require('../../infra/utils/s3');
 const ReadableStreamClone = require('readable-stream-clone');
 const { encryptStream } = require('../../infra/utils/stream');
 const KMS = require('../../infra/utils/kms');
+const Web3StorageService = require('./../../infra/utils/web3storage');
 const pinata = new Pinata();
 const s3 = new S3();
 const kms = new KMS();
+const web3Storage = new Web3StorageService();
 
 async function upload(file) {
   const { name, mimetype, data } = file;
+
   const dataKey = await kms.generateDataKey();
   console.log(dataKey);
   // upload to pinata
   const stream = Readable.from(data);
   const encryptedStreamPinata = Readable.from(
-    encryptStream(stream, dataKey.Plaintext),
+    encryptStream(stream, dataKey.Plaintext)
   );
   encryptedStreamPinata.path = name;
+
+  const encryptedStreamWeb3Storage = new ReadableStreamClone(
+    encryptedStreamPinata
+  );
+  encryptedStreamWeb3Storage.path = name;
+  console.log(file);
+  const files = await web3Storage.upload(file);
+  console.log({ files });
+  return;
+
   const encryptedStreamS3 = new ReadableStreamClone(encryptedStreamPinata);
   encryptedStreamS3.path = name;
   const pinataFile = await pinata.upload(encryptedStreamPinata, {
--- src/infra/utils/web3storage.js
+++ src/infra/utils/web3storage.js
@@ -1,9 +1,18 @@
-const Web3Storage = require('web3.storage');
+const { Web3Storage, Blob } = require('web3.storage');
 const config = require('./../../../config');
 
 class Web3StorageService {
   constructor() {
-    this.client = new Web3Storage(config.WEB3STORAGE_TOKEN);
+    console.log({ token: config.WEB3STORAGE_TOKEN });
+    this.client = new Web3Storage({ token: config.WEB3STORAGE_TOKEN });
+  }
+
+  async upload(file) {
+    let buffer = Buffer.from(file.data);
+    let arraybuffer = Uint8Array.from(buffer).buffer;
+    let blob = new Blob([arraybuffer], { type: file.mimetype });
+    const cid = await this.client.put([blob], { name: file.name });
+    return cid;
   }
 }
 
