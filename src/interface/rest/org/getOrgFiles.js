720594e94ec560a8e712cae222c9773b6bb4ed56
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2022-07-06 14:02:41 +0530

    Added mock data to get Org Files

--- src/interface/rest/org/getOrgFiles.js
+++ src/interface/rest/org/getOrgFiles.js
@@ -1,3 +1,4 @@
+const { File } = require('../../../domain');
 const { validator } = require('../middlewares');
 const { Joi, validate } = validator;
 
@@ -8,8 +9,11 @@ const getOrgFilesValidation = {
 };
 
 async function getOrgFiles(req, res) {
-  const { address } = req.params;
-  res.json({ address });
+  // const { address } = req.params;
+  const files = await File.getByAccount(
+    '0x6b8ddbA9c380e68201F76072523C4aC9AC4113ae'.toLowerCase(),
+  );
+  res.json({ files, total: files.length, matching: files.length });
 }
 
 module.exports = [validate(getOrgFilesValidation), getOrgFiles];
