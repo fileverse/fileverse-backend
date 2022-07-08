ab0cc28457c1f2c01e7ac87a9345c5a368a65bae
Sumit Kumar Singh <sumitks866@gmail.com>
2022-07-08 14:59:12 +0530

    fix

--- src/domain/org/analytics.js
+++ src/domain/org/analytics.js
@@ -1,21 +1,5 @@
-const { Log } = require('../../infra/database/models');
-
 async function analytics(address) {
-  const visits = await Log.aggregate([
-    {
-      $match: {
-        $expr: {
-          $gt: [
-            '$timeStamp',
-            { $dateSubtract: { startDate: '$$NOW', unit: 'week', amount: 1 } },
-          ],
-        },
-        eventName: 'view',
-      },
-    },
-  ]).count();
-
-  return { address, dataSet: visits };
+  return { address };
 }
 
 module.exports = analytics;
