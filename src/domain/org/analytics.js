bae0aef39b9f0bf7e6682e24841926f0ee8f5234
Sumit Kumar Singh <sumitks866@gmail.com>
2022-07-11 11:42:13 +0530

    get lastweek changes in analytics

--- src/domain/org/analytics.js
+++ src/domain/org/analytics.js
@@ -1,5 +1,33 @@
+const { Log } = require('../../infra/database/models');
+
+async function getEventInfo(eventName) {
+  const total = await Log.find({ eventName }).count();
+  const lastWeekTotal = await Log.aggregate([
+    {
+      $match: {
+        eventName,
+        $expr: {
+          $lt: [
+            '$timeStamp',
+            { $dateSubtract: { startDate: '$$NOW', unit: 'day', amount: 0 } },
+          ],
+        },
+      },
+    },
+    {
+      $count: 'lastWeekCount',
+    },
+  ]);
+  const changeFromLastWeek = lastWeekTotal[0]
+    ? total - lastWeekTotal[0].lastWeekCount
+    : '0';
+  return { total, changeFromLastWeek };
+}
+
 async function analytics(address) {
-  return { address };
+  const visits = await getEventInfo('view');
+  const downloads = await getEventInfo('download');
+  return { address, visits, downloads };
 }
 
 module.exports = analytics;
