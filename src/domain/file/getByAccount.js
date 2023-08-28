b451f3c7383912a52f81b2a6c63298da2fa87f50
Aakash-Jakhmola <aakash001jakhmola@gmail.com>
2023-08-28 11:51:32 +0530

    change sort order

--- src/domain/file/getByAccount.js
+++ src/domain/file/getByAccount.js
@@ -4,7 +4,7 @@ async function getByAccount({ address, offset, limit }) {
   const account = await Account.findOne({ address });
   const foundFiles = await File.aggregate([
     { $match: { owner: account._id } },
-    { $sort: { _id: -1 } },
+    { $sort: { createdAt: -1 } }, // Sort by createdAt latest to oldest
     { $skip: offset },
     { $limit: limit },
   ]);
