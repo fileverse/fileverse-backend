e3a52a47c389908b8fe6503c70cfca3119b6d284
Joshua Onwuzu <joshuaonwuzu75@gmail.com>
2023-11-13 10:38:59 +0100

    added apiKey to validateRecaptcha

--- src/interface/rest/middlewares/validateRecaptcha.js
+++ src/interface/rest/middlewares/validateRecaptcha.js
@@ -1,32 +1,55 @@
 const axios = require('axios');
+const crypto = require('crypto');
 const ErrorHandler = require('../../../infra/utils/errorHandler');
+
+const _isValidApiKey = (clientApiKey) => {
+  const apiKey = process.env.API_KEY;
+  if (!clientApiKey || clientApiKey.length !== apiKey.length) {
+    return false;
+  } else {
+    // compare keys in constant time to prevent potential timing attack
+    return crypto.timingSafeEqual(
+      Buffer.from(clientApiKey),
+      Buffer.from(apiKey),
+    );
+  }
+};
 
 async function validateRecaptcha(req, res, next) {
   const recaptchaSecret = process.env.RECAPTCHA_SECRET_KEY;
   const recaptchaToken = req.headers['recaptchatoken'];
-  if (!recaptchaToken)
-    return ErrorHandler.throwError({
-      code: 401,
-      message: `Recaptcha Token Missing`,
-      req,
-    });
-  const url = `https://www.recaptcha.net/recaptcha/api/siteverify?secret=${recaptchaSecret}&response=${recaptchaToken}`;
-  const threshold = 0.5;
-  const { data } = await axios.post(url);
-  if (
-    data.success &&
-    data.score &&
-    data.action &&
-    data.score >= threshold &&
-    (data.action === 'file' || data.action === 'lock' || data.action === 'edit')
-  ) {
+  const clientApiKey = req.headers['x-api-key'];
+  const isValidApiKey = _isValidApiKey(clientApiKey);
+  if (isValidApiKey) {
     next();
+    return;
   } else {
-    return ErrorHandler.throwError({
-      code: 403,
-      message: `Too Many Requests`,
-      req,
-    });
+    if (!recaptchaToken)
+      return ErrorHandler.throwError({
+        code: 401,
+        message: `Recaptcha Token Missing`,
+        req,
+      });
+    const url = `https://www.recaptcha.net/recaptcha/api/siteverify?secret=${recaptchaSecret}&response=${recaptchaToken}`;
+    const threshold = 0.5;
+    const { data } = await axios.post(url);
+    if (
+      data.success &&
+      data.score &&
+      data.action &&
+      data.score >= threshold &&
+      (data.action === 'file' ||
+        data.action === 'lock' ||
+        data.action === 'edit')
+    ) {
+      next();
+    } else {
+      return ErrorHandler.throwError({
+        code: 403,
+        message: `Too Many Requests`,
+        req,
+      });
+    }
   }
 }
 
